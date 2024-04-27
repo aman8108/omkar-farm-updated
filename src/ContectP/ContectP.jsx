04bfb8d6fda0f78fd91b8efe08e3b3a683082917
aman8108 <156769708+aman8108@users.noreply.github.com>
2024-04-27 11:24:36 +0530

    sskeriyblsvreitubsetu

--- src/ContectP/ContectP.jsx
+++ src/ContectP/ContectP.jsx
@@ -1,172 +1,218 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect } from "react";
 import { CiLocationOn } from "react-icons/ci";
 import { IoMdCall } from "react-icons/io";
 import { CiMail } from "react-icons/ci";
-import { Fade } from 'react-awesome-reveal';
-import ContectBanner from '../ContectBanner/ContectBanner';
-
-
-
+import { Fade } from "react-awesome-reveal";
+import ContectBanner from "../ContectBanner/ContectBanner";
+import { Button, Form, Input } from "antd";
+import $ from "jquery"
 
 function ContectP() {
-  useLayoutEffect(()=>{
-    window.scrollTo(0,0)
-   })
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  });
+
+  const submitForm = (values) => {
+    console.log("values",values);
+
+    var body = '<!DOCTYPE html><html><head><title>Enquiry Lead</title></head><body><div style="font-family:Arial,sans-serif;max-width:600px;margin:0 auto;background-color:#f2f2f2;padding:20px"><h2 style="color:#dc9d7e">Omkar Nature Farm</h2><p>Hello</p><p>Thank you for your interest in our products/services.</p><p>Please check your enquiry which generated from website:</p><table cellpadding="5" style="margin:0"><tr><td style="text-align:left;color:#dc9d7e"><strong>Name:</strong></td><td style="text-align:left">' + values.name + '</td></tr><tr><td style="text-align:left;color:#dc9d7e"><strong>Email:</strong></td><td style="text-align:left">' + values.email + '</td></tr><tr><td style="text-align:left;color:#dc9d7e"><strong>Subject:</strong></td><td style="text-align:left">' + values.subject + '</td></tr><tr><td style="text-align:left;color:#dc9d7e"><strong>Message:</strong></td><td style="text-align:left">' + values.message + '</td></tr></table><p style="font-weight:700">Best regards,<br>Your Team at Omkar Nature Farm</p></div></body></html>';
+
+        $.post("https://skdm.in/server/v1/send_lead_mail.php", {
+            toEmail: "amansinghthakur354@gmail.com",
+            fromEmail: "skdmlead@gmail.com",
+            bccMail: "skdmlead@gmail.com",
+            mailSubject: "New Customer Lead",
+            mailBody: body,
+            accountName: "Omkar nature farm",
+            accountLeadSource: "https://jutiepie.in/",
+            accountLeadName: values.name,
+            accountLeadEmail: values.email,
+            accountLeadPhone: values.contact,
+        },
+
+            function (dataa, status) {
+                console.log('data :' + dataa);
+                console.log("name:" + values);
+
+            });
+
+        alert("Your Form has Submitted Our team will contact with You  soon.");
+        values.preventDefault()
+        return false;
+  };
   return (
     <>
-     
-  <ContectBanner/>
+      <ContectBanner />
 
-  {/* ======================================= contact Us ================================== */}
-  <section id="contact" className="contact">
-    
-    <div className="titleSection aos-init aos-animate" data-aos="fade-right">
-      <Fade direction='right'>
-      <h1>CONTACT US</h1>
-      </Fade>
-     
-    </div>
-    <div className="container mb-5">
-      <div className="row">
-        <div className="col-lg-4 p-0 m-0">
-          <div className="contactCard">
-            <div className="contactIcon">
-              <span className="material-symbols-outlined"><CiLocationOn /></span>
+      {/* ======================================= contact Us ================================== */}
+      <section id="contact" className="contact">
+        <div
+          className="titleSection aos-init aos-animate"
+          data-aos="fade-right"
+        >
+          <Fade direction="right">
+            <h1>CONTACT US</h1>
+          </Fade>
+        </div>
+        <div className="container mb-5">
+          <div className="row">
+            <div className="col-lg-4 p-0 m-0">
+              <div className="contactCard">
+                <div className="contactIcon">
+                  <span className="material-symbols-outlined">
+                    <CiLocationOn />
+                  </span>
+                </div>
+                <div className="contactText">
+                  <p>
+                    At:Ambhai, post:kanchad, Tal:Wada, Wada, Maharashtra 421303
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="contactText">
-              <p>
-              At:Ambhai, post:kanchad, Tal:Wada, Wada, Maharashtra 421303
-
-              </p>
+            <div className="col-lg-4 p-0 m-0">
+              <div className="contactCard">
+                <div className="contactIcon">
+                  <span className="material-symbols-outlined">
+                    <IoMdCall />
+                  </span>
+                </div>
+                <div className="contactText">
+                  <a href="tel:+91 7028725777">
+                    <p>+91 8976256998</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 p-0 m-0">
+              <div className="contactCard">
+                <div className="contactIcon">
+                  <span className="material-symbols-outlined">
+                    <CiMail />
+                  </span>
+                </div>
+                <div className="contactText">
+                  <p>formomkarnature@gmail.com</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-lg-4 p-0 m-0">
-          <div className="contactCard">
-            <div className="contactIcon">
-              <span className="material-symbols-outlined"><IoMdCall /></span>
-            </div>
-            <div className="contactText">
-              <a href="tel:+91 7028725777">
-                <p>+91 8976256998</p>
-              </a>
-            </div>
-          </div>
+        <div className="titleSection mt-5">
+          <h4 style={{ fontSize: "2rem" }}>Omkar Nature Farm</h4>
         </div>
-        <div className="col-lg-4 p-0 m-0">
-          <div className="contactCard">
-            <div className="contactIcon">
-              <span className="material-symbols-outlined"><CiMail /></span>
-            </div>
-            <div className="contactText">
-              <p>formomkarnature@gmail.com</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="titleSection mt-5">
-      <h4 style={{ fontSize: "2rem" }}>HELLO THERE!</h4>
-    </div>
-    <div className="contactImgIcon">
-      <img
-        src="https://s.w.org/images/core/emoji/13.1.0/svg/1f44b.svg"
-        alt=""
-      />
-    </div>
-    <div className="container">
-      <div className="row  d-flex justify-content-center align-items-center">
-        <div className="col-md-12 col-lg-6 p-0 m-0">
-          <form
-            action=""
-            method="POST"
-            role="form"
-            className="php-email-form"
-            id="form"
-          >
-            <div className="row">
-              <div className="form-group col-md-6">
-                <label htmlFor="name">Your Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  className="form-control custName custNameValidation"
-                  id="name"
-                  required=""
-                />
-                <span className="CustNameError" style={{ color: "red" }} />
-              </div>
-              <div className="form-group col-md-6">
-                <label htmlFor="name">Phone</label>
-                <input
-                  type="number"
-                  className="form-control custPhone custPhoneValidation"
-                  name="subject"
-                  id="subject"
-                  required=""
-                />{" "}
-                <span className="MobileNoError" style={{ color: "red" }} />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Your Email</label>
-              <input
-                type="email"
-                className="form-control custEmail custEmailValidation"
-                name="email"
-                id="email"
-                required=""
-              />{" "}
-              <span className="EmailError" style={{ color: "red" }} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Message</label>
-              <textarea
-                className="form-control custMessage"
-                name="message"
-                rows={6}
-                required=""
-                defaultValue={" "}
-              />
-            </div>
-            <span className="MessageError" style={{ color: "red" }} />
-            <div className="my-3">
-              <div className="loading">Loading</div>
-              <div className="error-message" />
-              <div className="sent-message">
-                Your message has been sent. Thank you!
-              </div>
-            </div>
-            <div className="text-center cutomSubmit">
-              <button type="submit">Send Message</button>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div className="row d-flex justify-content-center align-content-center">
-        <div className="col-lg-12 col-md-12">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15025.20955219022!2d72.99306371534423!3d19.699746085695093!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be711ae47145489%3A0x1406918813b8acab!2sOmkar%20Nature%20Farm!5e0!3m2!1sen!2sin!4v1711437599023!5m2!1sen!2sin"
-
-            
-            width="100%"
-            height={400}
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
+        <div className="contactImgIcon">
+          <img
+            src="https://s.w.org/images/core/emoji/13.1.0/svg/1f44b.svg"
+            alt=""
           />
         </div>
-      </div>
-    </div>
-  </section>
-  {/* ===================================== footer ======================= */}
-  
-  {/* ==================================== Script Tag ======================== */}
-  {/* Google tag (gtag.js) */}
-</>
+        <div className="container">
+          <div className="row  d-flex justify-content-center align-items-center">
+            <div className="col-md-12 col-lg-6 p-0 m-0">
+              <Form
+                className="php-email-form"
+                layout="vertical"
+                onFinish={submitForm}
+              >
+                <div className="row">
+                  <div className="form-group col-md-6">
+                    <Form.Item 
+                    name="name" 
+                    label="Name"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please enter your name!',
+                      },
+                    ]}
+                    >
+                      <Input
+                        placeholder="Enter a name"
+                        className="form-control custName custNameValidation"
+                      />
+                    </Form.Item>
+                  </div>
+                  <div className="form-group col-md-6">
+                    <Form.Item 
+                    name="subject" 
+                    label="Subject"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please enter subject!',
+                      },
+                    ]}
+                    >
+                      <Input
+                        placeholder="Enter subject"
+                        className="form-control custPhone custPhoneValidation"
+                      />
+                    </Form.Item>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <Form.Item 
+                  name="email"
+                   label="Email"
+                   rules={[
+                    {
+                      required: true,
+                      message: 'Please enter email!',
+                    },
+                  ]}
+                   >
+                    <Input
+                      placeholder="Enter e-mail id"
+                      className="form-control custEmail custEmailValidation"
+                    />
+                  </Form.Item>
+                </div>
+                <div className="form-group">
+                  <Form.Item
+                   name="message"
+                    label="Message"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please Your message!',
+                      },
+                    ]}
+                    >
+                    <Input.TextArea
+                      className="form-control custMessage"
+                      placeholder="Enter message"
+                    />
+                  </Form.Item>
+                </div>
+                <div className="text-center cutomSubmit">
+                  <Form.Item>
+                    <Button htmlType="submit">Send Message</Button>
+                  </Form.Item>
+                </div>
+              </Form>
+            </div>
+          </div>
+          <div className="row d-flex justify-content-center align-content-center">
+            <div className="col-lg-12 col-md-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15025.20955219022!2d72.99306371534423!3d19.699746085695093!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be711ae47145489%3A0x1406918813b8acab!2sOmkar%20Nature%20Farm!5e0!3m2!1sen!2sin!4v1711437599023!5m2!1sen!2sin"
+                width="100%"
+                height={400}
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ===================================== footer ======================= */}
 
-  
-  )
+      {/* ==================================== Script Tag ======================== */}
+      {/* Google tag (gtag.js) */}
+    </>
+  );
 }
 
-export default ContectP
+export default ContectP;
